e369d59efb66c50721b30855710def86ccc9a737
Sandile Keswa <me@sandile.io>
2015-09-05 13:32:43 -0400

    projects

--- server/api/projects.js
+++ server/api/projects.js
@@ -4,4 +4,16 @@ module.exports = function(app) {
   app.get('/api/projects', function(req, res) {
     return res.status(200).json(data);
   });
+
+  app.get('/api/projects/:id', function(req, res) {
+    var matches = data.filter(function(project) {
+      return project._id === req.params.id;
+    });
+
+    if (!matches || matches.length < 1) {
+      return res.status(404).send();
+    } else {
+      return res.status(200).json(matches[0]);
+    }
+  });
 };
